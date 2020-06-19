6cc4bd46b3f9caabbcd8527e95f7213d931b76e9
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-06-19 10:08:49 +0530

    Create CPTTRN1.cpp

---
+++ CPTTRN1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    int r,c,i,j,tt;
+    cin>>tt;
+    char v[2]={'*','.'};
+    while(tt--)
+    {
+    cin>>r>>c;
+    for(i=1;i<=r;i++){
+        for(j=1;j<=c;j++)
+            cout<<v[(i+j)%2];
+        cout<<endl;}
+    }
+    return 0;
+}
